b2b4a47eb549243b8ffbd26d1dabc06e6a4567ba
AuroraPortesine <100139517+AuroraPortesine@users.noreply.github.com>
2022-05-02 16:07:25 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,11 @@
+#include "lib.h"
 
+float max(float num[], int dim){
+    float x=0;
+    for(int i=0; i<dim; i++){
+        if(x<num[i]){
+            x=num[i];
+        }
+    }
+    return x;
+}
