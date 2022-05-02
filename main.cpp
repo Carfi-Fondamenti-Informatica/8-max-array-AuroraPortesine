5f06f4b7697f66a308c7dfe1314b9b52c1f1c034
AuroraPortesine <100139517+AuroraPortesine@users.noreply.github.com>
2022-05-02 16:07:59 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    int dim;
+    cin >> dim;
+    float num[dim];
+    for(int i=0; i<dim; i++){
+        cin >> num[i];
+    }
+    cout << max(num, dim);
+    return 0;
 }
